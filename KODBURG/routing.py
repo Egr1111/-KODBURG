9441e9b54337557c7508a637b5b77a16c3ac9481
Egr1111 <yegor.kultin@bk.ru>
2023-05-27 02:29:10 +0300

    siudcs

--- KODBURG/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import consumer
-
-websocket_urlpatterns = [
-    path("ws/ac/<str:room_name>/", consumer.MyWebConsumer.as_asgi()),
-
-]
--- KODBURG/static/KODBURG/js/welcome.js
+++
@@ -1,7 +0,0 @@
-let container = document.querySelector(".container-fluid");
-let errors = document.querySelectorAll(".error");
-
-if (errors.length != 0){
-    container.scrollBy(0, container.scrollHeight)
-    console.log(errors)
-}
